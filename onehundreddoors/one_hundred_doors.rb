4fae2516f4d49acf11d80a0c34756358afc4a10a
pablo.silva <pablo.silva@locaweb.com.br>
2015-10-28 11:29:53 -0200

    Generate doors and visit them for the first time

--- onehundreddoors/one_hundred_doors.rb
+++ onehundreddoors/one_hundred_doors.rb
@@ -1,3 +1,17 @@
 class OneHundredDoors
+  def initialize
+    @doors = []
+  end
 
+  def visit_doors
+    @doors.each do |door|
+      door = 1
+    end
+  end
+
+  def generate_closed_doors
+    100.times do
+      @doors.push(0)
+    end
+  end
 end
--- onehundreddoors/one_hundred_doors_spec.rb
+++ onehundreddoors/one_hundred_doors_spec.rb
@@ -1,6 +1,29 @@
 require './one_hundred_doors'
 
 describe OneHundredDoors do
-  it 'do something' do
+  subject { described_class.new }
+
+  describe '#generate_closed_doors' do
+    it 'generate one hundred closed doors' do
+      subject.generate_closed_doors
+      doors = subject.instance_variable_get(:@doors)
+
+      expect(doors.size).to eql(100)
+      doors.each do |door|
+        expect(door).to eql(0)
+      end
+    end
+  end
+
+  describe '#visit_doors' do
+    context 'on the first visit through the doors' do
+      it 'open all of them' do
+        subject.visit_doors
+        doors = subject.instance_variable_get(:@doors)
+        doors.each do |door|
+          expect(door).to eql(1)
+        end
+      end
+    end
   end
 end
