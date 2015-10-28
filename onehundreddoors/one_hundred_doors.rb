9f3b5f2c15c8f6a9509a00e9ed0c509f3cf42ed6
pablo.silva <pablo.silva@locaweb.com.br>
2015-10-28 14:19:06 -0200

    My solution for one hundred doors problem

--- onehundreddoors/one_hundred_doors.rb
+++ onehundreddoors/one_hundred_doors.rb
@@ -4,14 +4,29 @@ class OneHundredDoors
   end
 
   def visit_doors
-    @doors.each do |door|
-      door = 1
+    visit = 1
+    @doors.each do
+      @doors.map!.with_index do |door, index|
+        if (index + 1) % visit == 0
+          toggle(door)
+        else
+          door
+        end
+      end
+      visit += 1
     end
+    require 'pry'; binding.pry
   end
 
   def generate_closed_doors
     100.times do
       @doors.push(0)
     end
+  end
+
+  private
+
+  def toggle(door)
+    if door == 0 then 1 else 0 end
   end
 end
--- onehundreddoors/one_hundred_doors_spec.rb
+++ onehundreddoors/one_hundred_doors_spec.rb
@@ -3,9 +3,12 @@ require './one_hundred_doors'
 describe OneHundredDoors do
   subject { described_class.new }
 
+  before do
+    subject.generate_closed_doors
+  end
+
   describe '#generate_closed_doors' do
     it 'generate one hundred closed doors' do
-      subject.generate_closed_doors
       doors = subject.instance_variable_get(:@doors)
 
       expect(doors.size).to eql(100)
@@ -16,13 +19,12 @@ describe OneHundredDoors do
   end
 
   describe '#visit_doors' do
-    context 'on the first visit through the doors' do
-      it 'open all of them' do
+    context 'on nth visit' do
+      let(:first_doors) { [1,0,0,1] }
+      it 'toggle all multiples of nth visit doors' do
         subject.visit_doors
         doors = subject.instance_variable_get(:@doors)
-        doors.each do |door|
-          expect(door).to eql(1)
-        end
+        expect(doors.slice(0,4)).to eql(first_doors)
       end
     end
   end
